b90a27a885504231fc88b75a04b00bd9f3f2fe8a
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-01-28 15:46:36 -0500

    requirements file removed ; added install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-#from setuptools import setup
 from distutils.core import setup, Extension
 import os
 
 
 setup(name='flipper',
       version='0.1',
-      description='Cosmology Analysis',
+      description='Map manipulation tools',
       author='Sudeep Das, Tobias Marriage',
       packages=['flipper'],
+      install_requires=['healpy==1.10.3',
+                        'numpy==1.11.3',
+                        'pyfits==3.4',
+                        'matplotlib==1.5.3',
+                        'scipy==0.18.1',
+                        'astLib==0.8.0'],
       zip_safe=False)
